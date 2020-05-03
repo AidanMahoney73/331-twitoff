684e38bc959a412dcd6d8d66597d9830fc50f5e6
Hathos_Vanox <49489547+AidanMahoney73@users.noreply.github.com>
2020-05-02 20:49:48 -0400

    Setup SQlite database

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # 331-twitoff
+
+# Setup
+
+Setup a database:
+
+```sh
+FLASK_APP=web_app flask db init
+FLASK_APP=web_app flask db migrate
+FLASK_APP=web_app flask db upgrade
+```
--- twitoff/__init__.py
+++ twitoff/__init__.py
@@ -2,11 +2,21 @@
 
 from flask import Flask
 
+from twitoff.models import db, migrate
 from twitoff.routes.home_routes import home_routes
 from twitoff.routes.book_routes import book_routes
 
+DATABASE_URI = "sqlite:///D:\\School\\unit3\\sprint3\\331-twitoff\\twitoff_development.db"
+
 def create_app():
     app = Flask(__name__)
+
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+
     app.register_blueprint(home_routes)
     app.register_blueprint(book_routes)
     return app
