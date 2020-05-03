8b0cc777ecc5520cecf5f8a4d802e6b4caa2d15b
Hathos_Vanox <49489547+AidanMahoney73@users.noreply.github.com>
2020-05-02 21:21:12 -0400

    Integrating with the db

--- twitoff/__init__.py
+++ twitoff/__init__.py
@@ -13,6 +13,7 @@ def create_app():
 
 
     app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     migrate.init_app(app, db)
 
