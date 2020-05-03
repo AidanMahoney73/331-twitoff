0afeb19b4f281e773f404c5d3ed24aa0b2968386
Hathos_Vanox <49489547+AidanMahoney73@users.noreply.github.com>
2020-05-03 02:28:32 -0400

    Basilica service fix

--- twitoff/services/basilica_service.py
+++ twitoff/services/basilica_service.py
@@ -7,9 +7,9 @@ load_dotenv()
 
 BASILICA_API_KEY = os.getenv("BASILICA_API_KEY")
 
-conn = basilica.Connection(BASILICA_API_KEY)
+connection = basilica.Connection(BASILICA_API_KEY)
 
-embedding = conn.embed_sentence('hey this is a cool tweet', model="twitter")
+embedding = connection.embed_sentence('hey this is a cool tweet', model="twitter")
 
 tweets = ["this is an example", "heres another fun tweet", "I don't really use twitter"]
 embeddings = connection.embed_sentences(tweets, model="twitter")
